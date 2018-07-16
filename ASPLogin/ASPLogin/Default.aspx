ddeaee612e06d9aed3c381c896ca7d81d687e245
babbincocks <zabbybab@gmail.com>
2018-07-16 18:27:06 -0400

    Uploading to switch computers, don't know if it works. Switched location of connection string to web.config

---
+++ ASPLogin/ASPLogin/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASPLogin.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="main.css" rel="stylesheet" />
+</head>
+<body style="height: 213px">
+    <form id="form1" runat="server">
+        <p class="indented">
+            &nbsp;</p>
+        <div>
+        <asp:Panel ID="Panel1" runat="server" BorderStyle="Solid" BorderWidth="1px">
+            
+                <p>
+                    Login</p>
+                <p>
+                    <asp:TextBox ID="txtLogin" runat="server"></asp:TextBox>
+                </p>
+                <br/>
+                <p>
+                    Password</p>
+                <p>
+                    <asp:TextBox ID="txtPass" runat="server"></asp:TextBox>
+                </p>
+                <p>
+                    <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+                </p>
+            
+                <p>
+                    &nbsp;</p>
+                <p>
+                    &nbsp;</p>
+                <p>
+                    <asp:TextBox ID="txtLoginAttempts" runat="server"></asp:TextBox>
+                    <asp:Label ID="Label1" runat="server" Text="# of Login Attempts"></asp:Label>
+                </p>
+            
+        </asp:Panel>
+            </div>
+    </form>
+</body>
+</html>
---
+++ ASPLogin/ASPLogin/Default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.Configuration;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASPLogin
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        int LoginAttempts = 1;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                if (txtLogin.Text.Any())
+                {
+                    if (txtPass.Text.Any())
+                    {
+                        //string connString = @"Server=PL1\MTCDB;Database=Sandbox;Trusted_Connection=True;" ;
+                        SqlConnection sqlConn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DesktopSchool"].ConnectionString);
+
+                        SqlDataAdapter sqlAdapt = new SqlDataAdapter("dbo.sp_AllUserInfo", sqlConn);
+
+                        DataTable dtUsers = new DataTable();
+
+                        sqlAdapt.Fill(dtUsers);
+
+                        DataRow[] result = dtUsers.Select("Username = '" + txtLogin.Text + "'");
+                        if(result.Length != 0)
+                        {
+                            if(result[0].ItemArray[1].ToString() == txtPass.Text)
+                            {
+                                Response.Write("We found your record! It says that you're smelly. That's unfortunate.");
+                            }
+                            else
+                            {
+                                Response.Write("Good news: you exist. Bad news: your password seems to be incorrect.");
+                            }
+                        }
+                        else
+                        {
+                            Response.Write("Login could not be found.");
+                        }
+                    }
+                    else
+                    {
+                        Response.Write("Please put in a password.");
+                    }
+                }
+                else if (txtPass.Text.Any())
+                {
+                    Response.Write("Please put in a login name.");
+                }
+                else
+                {
+                    Response.Write("Please put in your login information.");
+                }
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ViewState["Attempts"] != null)
+                {
+                    LoginAttempts = (int)ViewState["Attempts"] + 1;
+                }
+                txtLoginAttempts.Text = LoginAttempts.ToString();
+                ViewState["Attempts"] = LoginAttempts;
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
